c6d2dfd67c8c2e03375cf755d2249dfeda43b749
JeanEspindola <jeancarlo.deborbaespindola@hse24.de>
2020-10-10 21:12:05 +0200

    Adding SideDrawer. Adapting to mobile view.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,33 @@
-import React  from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.scss'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 export interface LayoutProps {
 	children: React.ReactNode
 }
 
-const Layout = (props: LayoutProps) => (
-	<Aux>
-		<Toolbar />
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</Aux>
-);
+class Layout extends Component {
+	state = {
+		showSideDrawer: true
+	}
+
+	sideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false })
+	}
+
+	render () {
+		return (
+				<Aux>
+					<Toolbar />
+					<SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+					<main className={classes.Content}>
+						{this.props.children}
+					</main>
+				</Aux>
+		)
+	}
+};
 
 export default Layout
