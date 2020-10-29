894161d384a6cda8a0ace0c7c99b9228bfb0597c
JeanEspindola <jeancarlo.deborbaespindola@hse24.de>
2020-10-29 19:09:13 +0100

    Adding downlevelIteration and absolute imports.

--- src/containers/Checkout/Checkout.tsx
+++ src/containers/Checkout/Checkout.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, RouteProps, RouteComponentProps } from 'react-router-dom'
-import CheckoutSummary from '../../components/CheckoutSummary/CheckoutSummary'
+import CheckoutSummary from 'components/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
-import { IngredientsType } from '../../utils/types'
+import { IngredientsType } from 'utils/types'
 
 export interface CheckoutProps {
 	history: RouteComponentProps['history']
@@ -23,10 +23,9 @@ class Checkout extends React.Component<CheckoutProps> {
 		const ingredients: IngredientsType = {}
 		let price = 0
 
-		// @ts-ignore
 		for (let param of query.entries()) {
 			if (param[0] === 'price') {
-					price = param[1]
+					price = Number(param[1])
 			} else {
 				ingredients[param[0]] = Number(param[1])
 			}
