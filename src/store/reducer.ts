6ee255804b08d64a1e7cfbbd6330c400078fd64e
JeanEspindola <jeancarlo.deborbaespindola@hse24.de>
2020-11-02 23:55:57 +0100

    Adding react-redux and redux dependencies. Adding initial action and reducer files.

---
+++ src/store/reducer.ts
@@ -0,0 +1,13 @@
+import * as actionTypes from './actions'
+import { AnyAction } from 'redux'
+
+const initialState = {
+	ingredients: null,
+	totalPrice: 4,
+}
+
+const reducer = (state = initialState, action: AnyAction) => {
+
+}
+
+export default reducer
