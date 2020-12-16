a9f492d099a6febfb83f088f1c4f08a52264a154
JeanEspindola <jeancarlo.deborbaespindola@hse24.de>
2020-12-16 18:27:45 +0100

    Covering Auth component.

--- src/containers/Auth/__tests__/Auth.tests.tsx
+++ src/containers/Auth/__tests__/Auth.tests.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { screen } from '@testing-library/react'
+import { Route } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 import { createDummyStore, WrappedRender } from 'tests/testUtils'
 import Auth from '../Auth'
 import { dummyRootAppState } from 'tests/testObjects/dummyRootState'
 import { dummyLocation } from '../../../tests/testObjects/dummyRouter'
+import { auth, setAuthRedirectPath } from '../../../redux/actions/authActions'
 
 
 describe('Auth', () => {
@@ -14,7 +17,7 @@ describe('Auth', () => {
 		path: '/auth',
 	}
 
-	test('renders correctly', () => {
+	test('renders correctly - not authenticated', () => {
 		WrappedRender(
 			<Auth
 				match={match}
@@ -36,5 +39,148 @@ describe('Auth', () => {
 		expect(password).toBeInTheDocument()
 		expect(submitButton).toBeInTheDocument()
 		expect(switchButton).toBeInTheDocument()
+	})
+
+	test('renders correctly - error', () => {
+		const newState = { ...state }
+		const { auth } = newState
+
+		const error = 'Error authenticating'
+
+		newState.auth = {
+				...auth,
+			error,
+		}
+
+		const newStore = createDummyStore(newState)
+
+		WrappedRender(
+				<Auth
+						match={match}
+						location={dummyLocation}
+				/>,
+				newStore,
+		)
+
+		const emailMsg = screen.getByText(error)
+		expect(emailMsg).toBeInTheDocument()
+	})
+
+	test('renders correctly - loading', () => {
+		const newState = { ...state }
+		const { auth } = newState
+
+		newState.auth = {
+			...auth,
+			loading: true,
+		}
+
+		const newStore = createDummyStore(newState)
+
+		WrappedRender(
+				<Auth
+						match={match}
+						location={dummyLocation}
+				/>,
+				newStore,
+		)
+
+		const loading = screen.getByText(/loading.../i)
+		expect(loading).toBeInTheDocument()
+	})
+
+	test('renders correctly - authenticated checks redirection', () => {
+		const newState = { ...state }
+		const { auth } = newState
+
+		newState.auth = {
+			...auth,
+			token: '1234xxx',
+			userId: '123',
+			authRedirectPath: '/checkout/redirect',
+		}
+
+		const newStore = createDummyStore(newState)
+
+		WrappedRender(
+				<>
+					<Auth
+							match={match}
+							location={dummyLocation}
+					/>
+					<Route path="/checkout/redirect">Test Redirect</Route>
+				</>,
+				newStore,
+		)
+
+		const redirect = screen.getByText(/test redirect/i)
+		expect(redirect).toBeInTheDocument()
+	})
+
+	test('click on the link and switches to sign in', () => {
+		WrappedRender(
+				<Auth
+						match={match}
+						location={dummyLocation}
+				/>,
+				store,
+		)
+
+		const switchLink = screen.getByRole('button', {
+			name: /switch to signin/i
+		})
+
+		expect(switchLink).toBeInTheDocument()
+
+		userEvent.click(switchLink)
+
+		expect(screen.queryByRole('button', {
+			name: /switch to signin/i
+		})).not.toBeInTheDocument()
+		expect(screen.getByRole('button', {
+			name: /switch to signup/i
+		})).toBeInTheDocument()
+	})
+
+	test('types an invalid on email input field', () => {
+		WrappedRender(
+				<Auth
+						match={match}
+						location={dummyLocation}
+				/>,
+				store,
+		)
+
+		const email = screen.getByPlaceholderText(/email address/i)
+
+		userEvent.type(email, 'abc@')
+
+		expect(screen.getByDisplayValue('abc@')).toBeInTheDocument()
+		expect(email).toHaveClass('Invalid')
+	})
+
+	test('fill and submit form', () => {
+		WrappedRender(
+				<Auth
+						match={match}
+						location={dummyLocation}
+				/>,
+				store,
+		)
+
+		const email = screen.getByPlaceholderText(/email address/i)
+		const password = screen.getByPlaceholderText(/password/i)
+		const submitButton = screen.getByRole('button', {
+			name: /submit/i
+		})
+
+		userEvent.type(email, 'test@test.de')
+		userEvent.type(password, '123456')
+
+		userEvent.click(submitButton)
+
+		expect(store.dispatch).toHaveBeenCalledWith(
+				auth('test@test.de', '123456', true)
+		)
 	})
 })
